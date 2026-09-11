131c56d96b3b97e2f78fb43f5d2fe3559f4e6ca5
bbftt <2861786825@qq.com>
2026-09-11 13:13:31 +0000

    updated by github action

---
+++ js/diytitle.js
@@ -0,0 +1,1 @@
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="w(ﾟДﾟ)w 不要走！再看看嘛！",clearTimeout(titleTime)):(document.title="♪(^∇^*)欢迎肥来！"+OriginTitile,titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
